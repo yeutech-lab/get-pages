dbfcf13e486f430e9f3e3633d2a89ae9e1b3e1bc
Dimitri Kopriwa <dimitri.kopriwa@kopaxgroup.com>
2017-12-06 02:40:29 +0700

    fix routes getting modified

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 /* eslint-disable no-shadow, no-param-reassign, no-unused-expressions */
 export default function getPages(routes, pages = {}) {
+  const copy = [...routes];
   function addRoutes(routes, parent) {
-    routes.forEach((route) => {
+    const innerCopy = [...routes];
+    innerCopy.forEach((route) => {
       parent[route.name] = route;
       route.childRoutes && addRoutes(route.childRoutes, route);
     });
   }
-  addRoutes(routes, pages);
+  addRoutes(copy, pages);
   return pages;
 }
 /* eslint-enable no-shadow, no-param-reassign, no-unused-expressions */

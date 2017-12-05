3f733963bb3e4d744e0a908994948ee56720e7db
Gitlab-CI <contact@kopaxgroup.com>
2017-12-05 19:38:09 +0000

    new release dist and lib

--- dist/get-pages.es.js
+++ dist/get-pages.es.js
@@ -1,12 +1,24 @@
+var toConsumableArray = function (arr) {
+  if (Array.isArray(arr)) {
+    for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) arr2[i] = arr[i];
+
+    return arr2;
+  } else {
+    return Array.from(arr);
+  }
+};
+
 function getPages(routes) {
   var pages = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
+  var copy = [].concat(toConsumableArray(routes));
   function addRoutes(routes, parent) {
-    routes.forEach(function (route) {
+    var innerCopy = [].concat(toConsumableArray(routes));
+    innerCopy.forEach(function (route) {
       parent[route.name] = route;
       route.childRoutes && addRoutes(route.childRoutes, route);
     });
   }
-  addRoutes(routes, pages);
+  addRoutes(copy, pages);
   return pages;
 }
 
